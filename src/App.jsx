b59f1974a14e6f9adc044f4ccdb07adb0b54edbf
Dheeraj Adhikri <adhkaridheeraj007@gmail.com>
2025-04-30 08:02:15 +0530

    fragment added to the code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item ">Dal</li>
+        <li class="list-group-item">Green Vegetable</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">salad</li>
+        <li class="list-group-item">milk </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
